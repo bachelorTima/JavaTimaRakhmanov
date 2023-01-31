df12a5702e294168d703f25fd07cd42d158cb4d7
Timurbek <timurbek.raxmanov@bk.ru>
2023-01-31 22:46:09 +0500

    Changed names

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Tima will be back");
+        System.out.println("Roma will be back");
+        System.out.println("Aziz will be back");
     }
 }
